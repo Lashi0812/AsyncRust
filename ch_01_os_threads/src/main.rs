7a679a011c278342b53374cd3d497ec4e1ed2c18
Lashi <lashi0812@gmail.com>
2024-10-18 08:33:23 +0000

    simple thread

--- ch_01_os_threads/src/main.rs
+++ ch_01_os_threads/src/main.rs
@@ -1,3 +1,22 @@
+use std::thread::{self,sleep};
+
 fn main() {
-    println!("Hello, world!");
+    println!("So ,we start the program here!");
+    let t1 = thread::spawn(move ||{
+        sleep(std::time::Duration::from_millis(200));
+        println!("Long running thread");
+    });
+
+    let t2 = thread::spawn(move ||{
+        sleep(std::time::Duration::from_millis(100));
+        println!("We can chain the callbacks...")        ;
+
+        let t3 = thread::spawn(move ||{
+            sleep(std::time::Duration::from_millis(50));
+            println!("... like this!");
+        });
+        t3.join().unwrap();
+    });
+    t1.join().unwrap();
+    t2.join().unwrap();
 }
